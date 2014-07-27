029343f6d96b58c74f6e0cda34faf2866ee90e8c
Stephen Heron <stephen.heron@gmail.com>
2014-07-27 20:28:38 +0100

    Integrated the edpug styling, strated integrating the winner status Javascript

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,72 +20,88 @@ twittercompApp.controller('TwitterUserCtrl', ['$scope', '$timeout', '$interval',
     {
       'name': 'David Manners',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/285282358/david_400x400.jpg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     },
     {
       'name': 'Bri',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/489160108774260736/PSv_GCo5_400x400.jpeg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amett'
     },
     {
       'name': 'Ali King',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/439185473571803136/-Gmvwc7w_400x400.jpeg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
     {
       'name': 'Thomas Dutrion',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/491894858810798081/EOS8PjU7_400x400.jpeg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
     {
       'name': 'Greg Dickson',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/429703437874262016/FsmslRN4_400x400.jpeg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
     {
       'name': 'Al Bennett',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/85281511/wallpaper3-1280x1024_400x400.jpg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
     {
       'name': 'Jordi Febrer',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/3658478259/c5543d8c8da9aef45d28eaa84d421bad_400x400.jpeg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
     {
       'name': 'James',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/213732946/twitter_400x400.jpg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
     {
       'name': 'Nev Stokes',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/1737771103/406696_10150481506191761_542356760_9044662_119149663_n_400x400.jpeg',
-      'tweet': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
   ];
 
+  $scope.winner = false;
+
+  var started = false;
+
   $scope.start = function() {
-    $scope.interval = $interval(function() {
+    if(started == false) {
 
-      var nonRemovedUsers = _.filter($scope.users, function(user) {
-        if(!user.removed && !user.highlighted) {
-          return user;
+      started = true;
+
+      $scope.interval = $interval(function() {
+
+        var nonRemovedUsers = _.filter($scope.users, function(user) {
+          if(!user.removed && !user.highlighted) {
+            return user;
+          }
+        });
+        
+        var nonHighLightedUsers = _.filter($scope.users, function(user) {
+          if(!user.removed) {
+            return user;
+          }
+        });
+
+        if(nonHighLightedUsers.length == 1) {
+          $interval.cancel($scope.interval);
+          $scope.winner = nonRemovedUsers[0];
+
+          return;
         }
-      });
 
-      _.each($scope.users, function(user) {
-        user.highlighted = false;
-      });
+        _.each($scope.users, function(user) {
+          user.highlighted = false;
+        });
 
-      var user = nonRemovedUsers[Math.floor(Math.random() * nonRemovedUsers.length)];
-      
-      user.highlighted = true;
+        var user = nonRemovedUsers[Math.floor(Math.random() * nonRemovedUsers.length)];
+        
+        user.highlighted = true;
 
-    }, 100);
+      }, 100);
+    }
   };
 
   $scope.stop = function() {
     if(angular.isDefined($scope.interval)) {
+
+      started = false;
+
       var timeout = randomIntFromInterval(100, 400);
       $timeout(function() {
         $interval.cancel($scope.interval);

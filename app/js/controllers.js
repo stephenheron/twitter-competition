f292db502526dd5dab5c3a97a8b6ddfc2f855597
Stephen Heron <stephen@line.uk.com>
2014-08-18 17:39:51 +0100

    Some tidying up to the css and js (Bad commit I know)

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,55 +14,73 @@ twittercompApp.directive('ngSpace', function() {
     }
 });
 
-twittercompApp.controller('TwitterUserCtrl', ['$scope', '$timeout', '$interval', function ($scope, $timeout, $interval) {
+twittercompApp.controller('TwitterUserCtrl', ['$scope', '$timeout', '$interval', '$http', function ($scope, $timeout, $interval, $http) {
 
   $scope.users = [
     {
       'name': 'David Manners',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/285282358/david_400x400.jpg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
     {
       'name': 'Bri',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/489160108774260736/PSv_GCo5_400x400.jpeg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
     {
       'name': 'Ali King',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/439185473571803136/-Gmvwc7w_400x400.jpeg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
     {
       'name': 'Thomas Dutrion',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/491894858810798081/EOS8PjU7_400x400.jpeg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
     {
       'name': 'Greg Dickson',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/429703437874262016/FsmslRN4_400x400.jpeg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
     {
       'name': 'Al Bennett',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/85281511/wallpaper3-1280x1024_400x400.jpg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
     {
       'name': 'Jordi Febrer',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/3658478259/c5543d8c8da9aef45d28eaa84d421bad_400x400.jpeg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
     {
       'name': 'James',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/213732946/twitter_400x400.jpg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
     {
       'name': 'Nev Stokes',
       'profile_image_url': 'https://pbs.twimg.com/profile_images/1737771103/406696_10150481506191761_542356760_9044662_119149663_n_400x400.jpeg',
+      'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue lorem, fermentum ut lorem eu'
     },
   ];
 
   $scope.winner = false;
 
   var started = false;
+  var firstRun = false
+
+  function firstRunFunction() {
+    alert($scope.hashtag);
+  }
 
   $scope.start = function() {
     if(started == false) {
 
       started = true;
+      if(firstRun === false) {
+        firstRun = true;
+        firstRunFunction();
+      }
 
       $scope.interval = $interval(function() {
 
@@ -97,24 +115,29 @@ twittercompApp.controller('TwitterUserCtrl', ['$scope', '$timeout', '$interval',
     }
   };
 
+  var stopping = false;
   $scope.stop = function() {
     if(angular.isDefined($scope.interval)) {
+      if(stopping === false) {
+        started = false;
+        stopping = true;
 
-      started = false;
+        var timeout = randomIntFromInterval(400, 700);
+        $timeout(function() {
+          $interval.cancel($scope.interval);
+      
+          _.each($scope.users, function(user) {
+            if(user.highlighted == true) {
+              user.removed = true;
+            }
+          
+            stopping = false;
+          });
 
-      var timeout = randomIntFromInterval(400, 700);
-      $timeout(function() {
-        $interval.cancel($scope.interval);
-    
-        _.each($scope.users, function(user) {
-          if(user.highlighted == true) {
-            user.removed = true;
-          }
-        });
+          $scope.start();
 
-        $scope.start();
-
-      }, timeout); 
+        }, timeout); 
+      }
     }
 
 
